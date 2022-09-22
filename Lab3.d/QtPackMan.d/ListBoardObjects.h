f77554be89cd86dfe5528fa6329d121d9fe1b9e9
michaelwiko1 <110108185+michaelwiko1@users.noreply.github.com>
2022-09-22 17:09:44 +0800

    Update ListBoardObjects.h

--- Lab3.d/QtPackMan.d/ListBoardObjects.h
+++ Lab3.d/QtPackMan.d/ListBoardObjects.h
@@ -7,24 +7,35 @@
 #define H_LIST_OF_OBJECTS
 typedef BoardObject *p_BoardObject;
 
-
+/*
+ * A class that store objects for the game
+ */
 class ListBoardObjects {
     std::vector <p_BoardObject> list;
     std::vector<p_BoardObject>::iterator internalIterator;
+    
 public:
+    /*
+     * a function for storing objects of the game
+     * @return theFirst value
+     */
     ListBoardObjects(p_BoardObject theFirst);
 
-    p_BoardObject theFirst();
+    p_BoardObject theFirst(); // method for storing first game memory
 
-    p_BoardObject theNext();
+    p_BoardObject theNext(); // method for storing next game memory
 
-    void addOne(p_BoardObject newOne);
+    /*
+     * a method for adding objects in the game
+     * @return newOne value
+     */
+    void addOne(p_BoardObject newOne); 
 
-    void removeOnePlayer();
+    void removeOnePlayer(); //method for removing the player
 
-    bool isThereAWall();
+    bool isThereAWall(); // method for checking if there's a wall
 
-    int isCookieValue();
+    int isCookieValue(); // method for retrieving cookie value or status
 };
 
 #endif
