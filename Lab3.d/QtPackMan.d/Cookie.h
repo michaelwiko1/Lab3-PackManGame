7a7da1b4cd5704c3b4723533b46c0ec8ddfa65cd
chenzhang9476 <92316933+chenzhang9476@users.noreply.github.com>
2022-09-22 13:58:36 +0800

    Update Cookie.h

--- Lab3.d/QtPackMan.d/Cookie.h
+++ Lab3.d/QtPackMan.d/Cookie.h
@@ -1,8 +1,8 @@
 #ifndef H_COOKIE
 #define H_COOKIE
-enum CookieState { VISIBLE, EATEN, EXPIRED};
+enum CookieState { VISIBLE, EATEN, EXPIRED}; //enum store the state
 
-enum PrizeValueIndex {CHEAP, MEDIUM, USEFUL, DEAR};
+enum PrizeValueIndex {CHEAP, MEDIUM, USEFUL, DEAR}; //enum store the prize value
 
 
 #include "BoardObject.h"
@@ -11,12 +11,18 @@ class Cookie : public BoardObject
 {
 
 private:
-	CookieState status;
+	CookieState status; 
 	ObjectColours color1;
 	ObjectColours  color2;
-	PrizeValueIndex valueIndex;
+	PrizeValueIndex valueIndex; // relating to the enum
 
 public:
+	/**
+	* Constructor
+	* The function initialize the colors and state and make the class as cookie.
+	* @param start_x starting point of x
+	* @param start_y starting point of y
+	*/
 	Cookie (int start_x, int start_y):BoardObject(start_x,start_y)
 		{ status=VISIBLE;
 		  color1 = YELLOW;
@@ -24,8 +30,10 @@ public:
 		  valueIndex=CHEAP;
                   myClass=A_COOKIE;
 		};
-
-	int get_value();
+	/**
+	*
+	*/
+	int get_value(); 
 
 	void touch ();
 };
