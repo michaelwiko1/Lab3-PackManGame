dc841ec4bd0d345eaef185343eb398c68409b42f
chenzhang9476 <92316933+chenzhang9476@users.noreply.github.com>
2022-09-22 13:44:21 +0800

    Update BoardObject.h

--- Lab3.d/QtPackMan.d/BoardObject.h
+++ Lab3.d/QtPackMan.d/BoardObject.h
@@ -1,6 +1,9 @@
 #ifndef H_BOARD_OBJECT
 #define H_BOARD_OBJECT
-
+/*
+* ObjectColours enum stores colours 
+* ObjectTypes enum stores the types
+**/
 enum ObjectColours {
     BLACK, YELLOW, BLUE, RED, INVISIBLE, BACKGROUND
 };
@@ -12,13 +15,19 @@ enum ObjectTypes {
 class BoardObject {
 
 private:
-    int x, y;
-    ObjectColours currentColor;
+    int x, y; /**< x and y value */
+    ObjectColours currentColor;  /**< a variable control the current color */
 protected:
-    ObjectTypes myClass;
+    ObjectTypes myClass; /**< a variable stores the type */
     //int unique_ID;
     //static int count;
 public:
+    /*
+    * The constructor
+    * The function initialize the type and color and coordination.
+    * @param start_x the starting point of x coordination
+    * @param start_y the starting point of y coordination
+    **/
     BoardObject(int start_x, int start_y) {
         x = start_x;
         y = start_y;
@@ -28,29 +37,50 @@ public:
     };
 
     //int get_ID() { return unique_ID;};
-
+    /*
+    * @return The x value
+    **/
     int get_x() {
         return x;
     };
-
+    /*
+    * @return The y value
+    **/
     int get_y() {
         return y;
     };
-
+    /*
+    * @return The current type
+    **/
     ObjectTypes get_Type() {
         return myClass;
     }
-
+    /*
+    * @return The current colour
+    **/
     ObjectColours get_Color() { return currentColor; }
 
+    /*
+    * The virtual function 
+    * Reset the color
+    **/
     virtual void touch() { currentColor = BACKGROUND; }
 
 protected:
-
+    /*
+    * @param new_color the new color
+    * Reset the color
+    **/
     void set_color(ObjectColours new_color) { currentColor = new_color; };
-
+    /*
+    * @param new_x the new x
+    * Reset the x
+    **/
     void set_x(int new_x) { x = new_x; };
-
+    /*
+    * @param new_y the new y
+    * Reset the y
+    **/
     void set_y(int new_y) { y = new_y; };
 
 
