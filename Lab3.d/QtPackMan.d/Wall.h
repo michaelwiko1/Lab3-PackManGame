b01e8ae3ab6fcfbf1e93615cb812c2ecbab3f408
michaelwiko1 <110108185+michaelwiko1@users.noreply.github.com>
2022-09-22 14:02:00 +0800

    Update Wall.h Comment

--- Lab3.d/QtPackMan.d/Wall.h
+++ Lab3.d/QtPackMan.d/Wall.h
@@ -1,4 +1,6 @@
-
+/*
+ * WalState enum stores the state of the wall (SOLID)
+ */
 enum WalState {
     SOLID
 };
@@ -8,13 +10,18 @@ enum WalState {
 class Wall : public BoardObject {
 
 private:
-    ObjectColours color1;
-
+    ObjectColours color1; // variable that controll
+/*
+ * Constructor
+ * Initialize the color of the Wall and wall class
+ */
 public:
     Wall(int start_x, int start_y) : BoardObject(start_x, start_y) {
-        color1 = BLACK;
+        color1 = BLACK; 
         myClass = A_WALL;
     };
-
+/*
+ * touch function give state to player (The wall is SOLID)
+ */
     void touch();
 };
