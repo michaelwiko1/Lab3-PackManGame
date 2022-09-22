8b7c63e0107d3b8455cb831e18465f7060dd3ebe
michaelwiko1 <110108185+michaelwiko1@users.noreply.github.com>
2022-09-22 17:03:43 +0800

    Update Model.h

--- Lab3.d/QtPackMan.d/Model.h
+++ Lab3.d/QtPackMan.d/Model.h
@@ -22,20 +22,26 @@ class Model : public QWidget {
 
 private:
 
-    int the_rows, the_cols;
+    int the_rows, the_cols; // to map the game with rows and columns
 
-    bool setOK;
-    bool gameOn;
-    int lives;
-    p_ListBoardObjects mySpace[ROWS][COLUMNS];
-    int player_id_x;
-    int player_id_y;
-    Player *startPlayer;
-    int currentScore;
-    Controller *myController;
+    bool setOK; // a boolean variable to prepare the game
+    bool gameOn; // a boolean variable to start a game
+    int lives; // a variable for player's life in interger
+    p_ListBoardObjects mySpace[ROWS][COLUMNS]; //a variable called mySpace to create ROWS and COLUMNS
+    int player_id_x; // player position x-axis
+    int player_id_y; // player position y-axis
+    Player *startPlayer; // a pointer to Player for start
+    int currentScore;// show value of the current score
+    Controller *myController; // a pointer to Controller
 
 
 public:
+    
+    /*
+     * Constructor
+     * @param rows is to set the mapping of the game (x-axis)
+     * @param columns is to set the mapping of the game (y-axis)
+     */
 
     Model(int rows, int cols) {
 
@@ -86,9 +92,14 @@ public:
         delete startPlayer;
     };*/
 
+    /*
+     * @return setOK value
+     */
     bool setModelOK() { return setOK; };
 
-
+    /*
+     * @return lives value (boolean)
+     */
     bool IamAlive() {
         if (0 < lives) {
             lives--;
@@ -98,17 +109,23 @@ public:
 
 public
     slots:
-            void move(char
-    command);
-
-    void paint();
+        /*
+         * function to player based on command
+         */
+        void move(char command);
+        /*
+         * function to color the game
+         */
+        void paint();
 
     signals:
-            void cellChanged(int
-    i,
-    int j,
-    char draw
-    );
+       /*
+	    * The function can change color
+	    * @param i is the coordination x of the target cell
+	    * @param j is the coordination y of the target cell
+	    * @param draw is the variable for character
+	    */
+        void cellChanged(int i, int j, char draw);
 };
 
 #endif
